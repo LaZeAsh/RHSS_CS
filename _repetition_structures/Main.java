fc5bb886457fd2aa1eb2de10deeb5626e090a155
LaZeAsh <ayushrgarg@gmail.com>
2022-10-24 14:30:54 -0400

    3.2

--- _repetition_structures/Main.java
+++ _repetition_structures/Main.java
@@ -4,15 +4,18 @@ class Main {
     static Console c;
     public static void main(String[] args) {
         c = new Console();
-    //    L2Q1_Echo(c); 
-    //    L2Q2_TempConverter2(c);
-    //    L2Q3_SellThisHouse(c);
-    //    L2Q4_ImTallerThanYou(c);
-    //    L2Q5_Marks2(c);
+       L2Q1_Echo(c); 
+       L2Q2_TempConverter2(c);
+       L2Q3_SellThisHouse(c);
+       L2Q4_ImTallerThanYou(c);
+       L2Q5_Marks2(c);
        L2Q6_PrimeNumbers(c);
-    //    L2Q7_TimeTables(c);
+       L2Q7_TimeTables(c);
     }
-
+    /**
+     * For loop to input numbers and print numbers that satisfy the condition
+     * @param c
+     */
     public static void L2Q1_Echo(Console c) {
         int num1 = 0;
         int column = 0;
@@ -33,15 +36,25 @@ class Main {
         }
     } // end of L21_Echo method
 
+    /**
+     * Converts the temp from celsius to farenheit 
+     * @param c
+     */
     public static void L2Q2_TempConverter2(Console c) {
-        // Getting input
-        c.println("Input your temperature in celsius (double)");
-        double celsiusTemp = c.readDouble();
+        int celsiusTemp = 0;
         //Calculation
-        double farenheitTemp = ((9/5)*celsiusTemp) + 32;
-        c.println(celsiusTemp + " in farenheit is " + farenheitTemp);
+        // For loop to find the farenheit equivalent of the temperature in 10º increments
+        for (int x = 0; x < 11; x++) {
+            double farenheitTemp = ((9/5)*celsiusTemp) + 32;
+            c.println(celsiusTemp + " in farenheit is " + farenheitTemp);
+            celsiusTemp += 10;
+        }
     } // end of L2Q2_TempConverter2 method
 
+    /**
+     * Finds the price of the house after a certain number of weeks
+     * @param c
+     */
     public static void L2Q3_SellThisHouse(Console c) {
         // Defining / Input
        int initialPrice = 427000;
@@ -54,8 +67,12 @@ class Main {
         }
         initialPrice = (int) Math.round(initialPrice * 0.95);
        }
-    }
+    } // end of L2Q3_SellThisHouse method
 
+    /**
+     * Finds the tallest height out of the heights inputted
+     * @param c
+     */
     public static void L2Q4_ImTallerThanYou(Console c) {
         double height = 0;
         double shortestHeight = 0;
@@ -82,9 +99,9 @@ class Main {
         c.println("The shortest height was " + shortestHeight);
         c.println("The tallest height was " + tallestHeight);
         c.println("The shortest height was " + averageHeight);
-    }
+    } // end of L2Q4_ImTallerThanYou method
     /**
-     * Pseudo code needed
+     * Generates marks for the # of students inputted and calculates their average
      * @param c
      */
     public static void L2Q5_Marks2(Console c) {
@@ -104,29 +121,32 @@ class Main {
 
        // OUTPUT
        c.println("The average mark in the class were " + average);
-    }
+    } // end of L2Q5_Marks2 method
     /**
-     * Pseudo code needed
+     * Gets all the prime numbers between 2 - 100 using 2 for loops
      * @param c
      */
     public static void L2Q6_PrimeNumbers(Console c) {
+        // Processing and output
+        // For loop to check which number is a prime number
         for(int x = 2; x < 101;x++) { 
-            for (int i = 1; i < 101; i++) {
-                if(x % i == 0) {
-                    break;
-                }
-                if(x % i == 0) {
+            // for loop to iterate through the divisors
+            for (int i = 2; i < 101; i++) {
+                if(x % i == 0 && x != i || x == 2) {
                     System.out.println(x + " is not a prime number");
                     break;
                 }
                 if(i >= x) {
                     c.println(x + " is a prime number");
+                    break;
                 }
-                
-             }
+            }
         } 
-    }
-
+    } // end of L2Q6_PrimeNumbers method
+    /**
+     * Answer for Question 7, prints time tables
+     * @param c
+     */
     public static void L2Q7_TimeTables(Console c) {
         // For loop for printing the time tables
         // Input Processing and Output
